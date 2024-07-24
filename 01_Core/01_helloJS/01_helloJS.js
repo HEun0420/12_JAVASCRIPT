6f4b409d978fb16086d5eb43b6b0bf5602ef238f
HEun0420 <usialeta@gmail.com>
2024-07-24 11:10:52 +0900

    javascript

---
+++ 01_Core/01_helloJS/01_helloJS.js
@@ -0,0 +1,32 @@
+console.log('hellowoerld');
+
+// 코드러너 단축키
+// ctrl + alt + n
+
+// ctrl + / : 한줄주석
+/* alt + shift + a: 여러줄 주석 */
+// ctrl+ shift+ e : 파일 목록탭과 eplorer 탭 이동 단축키
+// ctrl+ shift+ p: 컨맨드 팔렛트
+
+console.log('안녕하세요');
+
+console.log('');
+
+
+// 파일 생성 및 폴더 생성 단축키 설정
+// 커맨드 파레트 -> preferences:open keyboard shortcut (json)
+/* 
+[
+  { "key": "ctrl+n",
+    "command": "explorer.newFile",
+    "when": "!editorFocus" },
+
+
+  { "key": "ctrl+shift+n",
+    "command": "explorer.newFolder",
+    "when": "!editorFocus" }
+]
+*/
+
+
+
