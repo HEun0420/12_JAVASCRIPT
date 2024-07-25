5c57810e2c522bc02d78039b54d2119640f8d8d4
HEun0420 <usialeta@gmail.com>
2024-07-25 16:48:44 +0900

    JavaScript

---
+++ 01_Core/10_array/01_array.js
@@ -0,0 +1,18 @@
+// 배열
+
+// 배열 리터럴을 통해 배열 생성
+// 자바스크립트의 모든 값이 배열의 요소가 될 수 있다.
+const arr = ['바나나', '복숭아', '키위']
+console.log(arr);
+
+
+
+// 배열 생성자 함수
+const arr2 = new Array();
+console.log(arr2);
+
+const arr3 = new Array(10);
+console.log(arr3);
+
+
+
