5134e2d9232b5dd69c52b2aa6ed274ab7e5eef02
HEun0420 <usialeta@gmail.com>
2024-07-29 16:26:20 +0900

    dom

---
+++ 01_Core/09_script-mode/04_change-case.js
@@ -0,0 +1,15 @@
+// 변화 상황
+
+// 1. 일반 함수의 this
+(function(){
+    'use strict'
+
+    // 염격모드에서는 일반 함수의 this 사용을 제한하고
+    // 생성자 함수의 this만 허용한다.
+    function test(){
+        console.log(this);
+    }
+    test(); // undefined
+
+    new test(); // testP{}
+}())
